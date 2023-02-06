625ec9d55d4f47e35fb8cc6bbcb2cd219c5aa078
CleytonSM <122110138+CleytonSM@users.noreply.github.com>
2023-02-06 11:56:53 -0300

    Transeferindo as categorias para a página de alimentos

--- dia5.js
+++ dia5.js
@@ -1,37 +1,31 @@
-window.onload = function() {
-    let frutas = [];
-    let doces = [];
-    let congelados = [];
-    let laticineos = [];
-    
-    const input = document.querySelector('input');
-    const frutasList = document.getElementById('fruta');
-    const docesList = document.getElementById('doce');
-    const congeladosList = document.getElementById('congel')
-    const laticineosList = document.getElementById('lat')
-    const submitBtn = document.querySelector('#add')
-    
-    submitBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      const nomeDoItem = input.value;
-      const tipoDoItem = prompt(`Que tipo de alimento é ${nomeDoItem}?`);
-  
-      if (tipoDoItem === 'Frutas') {
-        frutas.push(nomeDoItem);
-        frutasList.textContent = `${frutas.join(', ')}`;
-      } else if (tipoDoItem === 'Doces') {
-        doces.push(nomeDoItem);
-        docesList.textContent = `${doces.join(', ')}`;
-      } else if (tipoDoItem === 'Laticíneos'){
-        laticineos.push(nomeDoItem);
-        laticineosList.textContent = `${laticineos.join(', ')}`;
-      } else if (tipoDoItem === 'Congelados'){
-        congelados.push(nomeDoItem);
-        congeladosList.textContent = `${congelados.join(', ')}`
-      }
+window.addEventListener('load', function(){
+    // variáveis
+    let armazenando = [];
 
-  
-      input.value = '';
 
-  });
-}
+    // constantes lista
+    const armazemList = document.getElementById('armazena-categorias');
+    const input = document.getElementById('categorias');
+    const addBnt = document.getElementById('adicionar');
+    const finishBnt = document.getElementById('finalizar');
+
+    // o botão adicionar está adicionando as categorias
+    addBnt.addEventListener('click', (event) => {
+        event.preventDefault();
+        const nomeDoItem = input.value;
+        if (input.value == 0){
+            alert('[ERRO] Adicione um valor');
+        } else{
+            armazenando.push(nomeDoItem);
+            armazemList.textContent = `${armazenando.join(', ')}`;
+        }
+        
+    })
+    
+    // o botão finish está redirecionando para a nova aba
+    finishBnt.addEventListener('click', (event) => {
+        localStorage.setItem('categorias', JSON.stringify(armazenando));
+        window.location.href = 'produtos.html';
+     })
+})
+
